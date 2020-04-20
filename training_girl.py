6fed502970365cf5d7a1740ca36417bc86c01109
Aman Garg <43947335+aman-garg0001@users.noreply.github.com>
2020-04-20 16:56:17 +0530

    Training RNN for girl name prediction

--- training_girl.py
+++ training_girl.py
@@ -2,25 +2,13 @@ import pandas as pd
 import numpy as np
 
 data = pd.read_csv('girl_names.csv', index_col = False,  names = ['Name'], engine='python')
-data_boy = []
-for i, j in data.iterrows():
-  data_boy.append(j['Name'])
-  
-lis = []
-for i in range(len(data_boy)):
-  j = data_boy[i]
-  j = j.split(' ')
-  j = j[0].split("'")
-  j = j[0].split('�')
-  lis.extend(j)
+data_boy = data['Name']
 
-data_boy = [x.lower() + '.' for x in lis]
+import pandas as pd
+import numpy as np
 
-lis = []
-for j in data_boy:
-  chars = list(j)
-  lis.extend(chars)
-lis = set(lis)
+data = pd.read_csv('girl_names.csv', index_col = False,  names = ['Name'], engine='python')
+data_boy = data['Name']
 
 char_to_index = dict( (chr(i+96), i) for i in range(1,27))
 char_to_index[' '] = 0
@@ -48,7 +36,6 @@ for i in range(m):
             
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import LSTM, Dense
-from tensorflow.python.keras.callbacks import LambdaCallback
 
 model = Sequential()
 model.add(LSTM(128, input_shape=(max_char, char_dim), return_sequences=True))
@@ -56,8 +43,6 @@ model.add(Dense(char_dim, activation='softmax'))
 
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-name_generator = LambdaCallback(on_epoch_end = generate_name_loop)
-
-model.fit(X, Y, batch_size=64, epochs=500, callbacks=[name_generator], verbose=1)
+model.fit(X, Y, batch_size=64, epochs=500, verbose=1)
 
 model.save('girl_name.h5')
